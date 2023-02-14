10b1ac87a6e1714f6b6dea243f68e7a2400df25a
@omi-coder <omi-ufa@yandex.ru>
2023-02-14 12:06:28 +0300

    first commit

---
+++ StoreTECH/settings.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+
+from envparse import env
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if (env_path := BASE_DIR.joinpath('.env')) and env_path.is_file():
+    env.read_envfile(env_path)
+
+SECRET_KEY = env.str('SECRET_KEY')
+
+DEBUG = env.bool('DEBUG', default=False)
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'user',
+    'drf_spectacular',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'StoreTECH.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'StoreTECH.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
+        'PORT': env.int('POSTGRES_PORT', default=5432),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+AUTH_USER_MODEL = 'user.User'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Todolist API',
+    'DESCRIPTION': 'Awesome todolist project',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'POSTPROCESSING_HOOKS': []
+}
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ user/views.py
@@ -0,0 +1,84 @@
+from django.contrib.auth import (
+    get_user_model,
+    login,
+    logout
+)
+from drf_spectacular.utils import extend_schema
+from rest_framework import (
+    permissions,
+    status
+)
+from rest_framework.generics import (
+    CreateAPIView,
+    GenericAPIView,
+    RetrieveUpdateDestroyAPIView,
+    UpdateAPIView
+)
+from rest_framework.response import Response
+
+from user.serializers import (
+    LoginSerializer,
+    ProfileSerializer,
+    RegistrationSerializer,
+    UpdatePasswordSerializer
+)
+
+
+USER_MODEL = get_user_model()
+
+
+class RegistrationView(CreateAPIView):
+    """ Регистрация пользователя. """
+    model = USER_MODEL
+    serializer_class = RegistrationSerializer
+
+
+class LoginView(GenericAPIView):
+    """ User login. """
+    serializer_class = LoginSerializer
+
+    @extend_schema(
+        description="User login",
+        summary="User login"
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
+
+
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    """ Профиль пользователя. """
+    serializer_class = ProfileSerializer
+    queryset = USER_MODEL.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    @extend_schema(
+        description="Retrieve user info",
+        summary="User info"
+    )
+    def get_object(self):
+        return self.request.user
+
+    @extend_schema(
+        description="Destroy user",
+        summary="Destroy user"
+    )
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UpdatePasswordView(UpdateAPIView):
+    """ Обновление пароля. """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    @extend_schema(
+        description="Update user",
+        summary="Update"
+    )
+    def get_object(self):
+        return self.request.user
